19b6c31391b648bcdb271f658445aeae36c11ada
Percegaroli <bruno.percegaroli@gmail.com>
2021-05-17 20:15:56 -0300

    detect user preferd theme

--- src/components/context/ThemeContext/Provider.tsx
+++ src/components/context/ThemeContext/Provider.tsx
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { INITIAL_THEME_VALUE, ThemeContext } from '.';
 import { Theme } from './interface';
 
 const ThemeProvider: React.FC = ({ children }) => {
   const [theme, setTheme] = useState<Theme>(INITIAL_THEME_VALUE.theme);
+
+  const getUserPreferedTheme = () => {
+    if (typeof window !== 'undefined') {
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        return 'DarkTheme';
+      }
+    }
+    return 'LightTheme';
+  };
+
+  useEffect(() => {
+    setTheme(getUserPreferedTheme());
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ theme, changeTheme: setTheme }}>
